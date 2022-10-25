f82d1ece96d5b2588c6bae8b722d54aa7bef3391
Nick Saenko <nick.saenko@hs-osnabrueck.de>
2022-10-25 13:48:25 +0200

    Die createPopUp-Funktion wurde ausgelagert. Siehe "popUpInformation.js"

--- js/popUpInformation.js
+++ js/popUpInformation.js
@@ -1,1 +1,146 @@
-//Hier kommt die function "createPopUp" rein.
+export function createPopUp(name){
+
+    var result;
+
+    switch (name) {
+        case AA:
+
+            
+            break;
+
+        case AB:
+            
+        break;
+
+        case AC:
+            
+            break;
+
+        case AD:
+            
+        break;
+
+        case AE:
+            
+            break;
+
+        case AF:
+            
+        break;
+
+        case BA:
+            
+            break;
+
+        case BB:
+            
+        break;
+
+        case BC:
+            
+            break;
+
+        case BIB:
+
+           
+            
+        break;
+
+        case Mensa:
+            result = "<h1>Mensa</h1>" + 
+                    "<img src=https://www.arcguide.de/wp-content/uploads/2/8/28704.jpg' width='300' height='200'>" +
+                   
+                    "<table>" +
+
+                    "<tr>" +
+                
+                    "<th> <i class='fa fa-cutlery'></i> </th>" +
+                    "<th> <div class='textNebenIconPlatzierung'>Mensa & Cafeteria</div> </th>" +
+                   
+                    "</tr>" + 
+
+                    "<tr>" +
+                
+                    "<th> <i class='fa fa-credit-card-alt'></i> </th>" +
+                    "<th> <div class='textNebenIconPlatzierung'>Validierungsautomat</div> </th>" +
+                   
+                    "</tr>" + 
+                
+                    "</table>"
+                    ;
+            
+            break;
+
+        case MA:
+            
+        break;
+
+        case SB:
+            
+            break;
+
+        case SC:
+            
+        break;
+
+        case SD:
+            
+            break;
+
+        case SE:
+            
+        break;
+
+        case SF:
+            
+            break;
+
+        case SG:
+            
+        break;
+
+        case SH:
+            
+            break;
+
+        case SJ:
+            
+        break;
+
+        case SI:
+            
+            break;
+
+        case SK:
+            
+        break;
+
+        case SL:
+            
+            break;
+
+        case SN:
+            
+        break;
+
+        case SO:
+            
+            break;
+
+        case SP:
+            
+        break;
+
+        case SQ:
+            
+            break;
+
+        case SR:
+            
+        break;
+    
+        
+    }
+
+    return result;
+}
