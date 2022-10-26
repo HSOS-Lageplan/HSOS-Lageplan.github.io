199337dc74949b3cdeef9bb16ad0a32ab8a9c143
Evelyn Hasenpusch <evelyn.hasenpusch@hs-osnabrueck.de>
2022-10-26 12:12:22 +0200

    Fragen und Antworten

--- js/answers.js
+++ js/answers.js
@@ -3,51 +3,51 @@ function getAnswers(id) {
 
     switch (id) {
         case 0:
-            answers = ["grün", "green"];
+            answers = ["Ride 2 Repair", "Ride2Repair", "R2R"];
             break;
         case 1:
-        
+            answers = ["Buch", "Bücher", "Book", "Books", "Ein Buch", "A book"]
             break;
         case 2:
-    
+            answers = ["35368", "35.368", "35,368"]
             break;
         case 3:
-
+            answers = ["HAR315"]
             break;
         case 4:
-
+            answers = ["Toten"]
             break;
         case 5:
-
+            answers = ["SB0437", "SB 0437", "0437", "437"]
             break;
         case 6:
-
+            answers = ["Uhr", "Eine Uhr", "Clock", "A clock"]
             break;
         case 7:
-
+            answers = ["Fotolabor"]
             break;
         case 8:
-
+            answers = ["34"]
             break;
         case 9:
-
+            answers = ["AF"]
             break;
         case 10:
-
+            answers = ["4", "6"]
             break;
         case 11:
-
+            answers = ["AD 0039", "AD0039", "0039", "39"]
             break;
         case 12:
-
+            answers = ["SG"] //Hier wäre die Möglichkeit für Standort-Abfrage
             break;
         case 13:
-
+            answers = ["Giraffe"]
             break;
         case 14:
-
+            answers = ["Stein", "Felsen", "Stone", "Boulder"]
             break;
-                        
+            answers = ["Mensa"] //Das Finale ist noch nicht fertig          
         default:
             break;
     }
