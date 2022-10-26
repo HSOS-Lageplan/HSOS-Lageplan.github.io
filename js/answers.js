9d337bda806f5a63ab2c0657a79a247eb0dfd86f
Henry Wulff <henry.wulff@hs-osnabrueck.de>
2022-10-26 11:39:02 +0200

    schnitzeljagd answers

---
+++ js/answers.js
@@ -0,0 +1,56 @@
+function getAnswers(id) {
+    var answers = [];
+
+    switch (id) {
+        case 0:
+            answers = ["grün", "green"];
+            break;
+        case 1:
+        
+            break;
+        case 2:
+    
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;
+        case 10:
+
+            break;
+        case 11:
+
+            break;
+        case 12:
+
+            break;
+        case 13:
+
+            break;
+        case 14:
+
+            break;
+                        
+        default:
+            break;
+    }
+
+    return answers;
+}
--- js/schnitzeljagd.js
+++ js/schnitzeljagd.js
@@ -3,3 +3,29 @@ if(sessionStorage.getItem("progress" == null)) {
     sessionStorage.setItem("progress", "0");
 }
 
+function toggleSidebar() {
+    var ele = document.getElementById("question-window");
+
+    if(ele.className == "active") {
+        ele.className = "";
+    }
+    else {
+        ele.className = "active";
+    }
+}
+
+function checkAnswer(id) {
+    var ele = document.getElementsByClassName("answer");
+    ele = ele[id];
+
+    var answers = getAnswers(id);
+
+    for (let i = 0; i < answers.length; i++) {
+        if(answers[i].toUpperCase() === ele.value.toUpperCase()) {
+            ele.className = ele.className + " correct";
+            ele.readOnly = true;
+            sessionStorage.setItem("progress", id + 1);
+        }     
+    }
+}
+
